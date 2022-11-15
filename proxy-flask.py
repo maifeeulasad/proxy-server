22585392f2c36caa65fbb9de4e1ee7352b106f0c
Maifee Ul Asad <29339330+maifeeulasad@users.noreply.github.com>
2022-11-15 15:32:06 +0600

    [feature]: cors

--- proxy-flask.py
+++ proxy-flask.py
@@ -1,16 +1,17 @@
 from flask import Flask, Response, request
+from flask_cors import CORS, cross_origin
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/proxy", methods=["POST"])
+@cross_origin()
 def read_and_send():
         url = request.get_json()["url"]
         res = requests.get(url)
         html = res.text
         return Response(html, mimetype='text/html')
-
-#print(read_and_send("https://www.cnn.com/2022/11/14/business/jeff-bezos-charity/index.html"))
 
 if __name__ == "__main__":
         app.run()
